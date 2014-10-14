33f8c0a130ff48f164650de78b21e19207ac7622
Alexandre Lucchesi <alexandrelucchesi@gmail.com>
2014-10-14 15:14:36 -0300

    Working version considering power of 2 input (numbers and processes).

--- main.c
+++ main.c
@@ -16,17 +16,20 @@
 #include <mpi.h>
 
 
-#define DEBUG_MAIN
+//#define DEBUG_MAIN
 //#define DEBUG_SCATHER
+//#define DEBUG_REDUCE
+//#define DEBUG_REDUCE_SUMTREE
 
 
 typedef unsigned long long int ulli;
 
 
 void generate_floats(FILE* out, int count);
-int reduce(int my_rank, int comm_sz, unsigned int count, float nums[], float *sum);
+int reduce(int my_rank, int comm_sz, float num, float *sum);
 int reduce_sumtree(int my_rank, int comm_sz, unsigned int count, float nums[], float *sum);
-void get_data(int my_rank, int comm_sz, unsigned int *my_count, float **my_nums);
+void scather(int my_rank, int comm_sz, unsigned int *my_count, float **my_nums);
+void scather_intercalate(int my_rank, int comm_sz, unsigned int *my_count, float **my_nums);
 static void usage(int argc, char *argv[]);
 
 
@@ -45,13 +48,9 @@ void generate_floats(FILE* out, int count) {
 } /* generate_floats */
 
 
-int reduce(int my_rank, int comm_sz, unsigned int count, float nums[], float *sum) {
-
-    // Calculates given range.
-    (*sum) = 0.0f;
-    for (int i = 0; i < count; i++) {
-        (*sum) += nums[i];
-    }
+int reduce(int my_rank, int comm_sz, float num, float *sum) {
+    // Initializes `*sum`.
+    *sum = num;
 
     if (comm_sz == 1)
         return 0;
@@ -71,16 +70,16 @@ int reduce(int my_rank, int comm_sz, unsigned int count, float nums[], float *su
                 if (op > 0) { /* Receive */
                     int sender = j + pow(2, step);
                     float his_sum;
-                    MPI_Recv(&his_sum, 1, MPI_FLOAT, sender, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    MPI_Recv(&his_sum, 1, MPI_FLOAT, sender, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                     (*sum) += his_sum;
-#ifdef DEBUG
+#ifdef DEBUG_REDUCE
                     printf("[Step %d] : my_rank %d - Receive from %d - his_sum is %.2f and *sum is %.2f\n", step, my_rank, sender, his_sum, *sum);
 #endif
                     break;
                 } else { /* Send */
                     int receiver = j - pow(2, step);
-                    MPI_Send(sum, 1, MPI_FLOAT, receiver, 2, MPI_COMM_WORLD);
-#ifdef DEBUG
+                    MPI_Send(sum, 1, MPI_FLOAT, receiver, 3, MPI_COMM_WORLD);
+#ifdef DEBUG_REDUCE
                     printf("[Step %d] : my_rank %d - Send to %d - *sum is %.2f\n", step, my_rank, receiver, *sum);
 #endif
                     finish = 1; // After sending its results, the process returns.
@@ -94,67 +93,113 @@ int reduce(int my_rank, int comm_sz, unsigned int count, float nums[], float *su
     return 0;
 }
 
+
 int reduce_sumtree(int my_rank, int comm_sz, unsigned int count, float nums[], float *sum) {
-    *sum = 0.0f;
+    if (count == 1) { // Process has only one element.
+        return reduce(my_rank, comm_sz, nums[0], sum);
+    } else {
+        ldiv_t res = ldiv(count, 2);
+        unsigned int qty = res.quot;
 
-    // If there's only one process, calculates the sum sequentially.
-    if (comm_sz == 1) {
-        for (int i = 0; i < count; i++)
-            (*sum) += nums[i];
-        return 0;
-    }
+        float my_nums[qty];
+        float his_nums[qty];
+        float res_nums[qty];
 
-    int n_proc = comm_sz;
-    for (int finish = 0, step = 0; n_proc > 1 && !finish; step++) {
-        div_t res = div(n_proc, 2);
-        int n_iter = res.rem == 0 ? n_proc : n_proc - 1;
-        // Number of 'active' processes, i.e., how many processes are
-        // participating in the communication at this step.
-        n_proc = res.quot + res.rem;
+        if (my_rank % 2 == 0) {
+            int dst = my_rank + 1;
 
-        // i: controls the number of iterations;
-        // j: current process index;
-        // k: index of the next process' number to be sent;
-        // op: whether the operation to be performed is
-        //    MPI_Send() (negative) or MPI_Recv() (positive).
-        for (int i = 0, j = 0, k = 0, op = 1; i < n_iter; i++, j = i * pow(2, step)) {
-            if (my_rank == j) {
-                if (op > 0) { /* Receive */
-                    int sender = j + pow(2, step);
-                    float his_sum;
-                    MPI_Recv(&his_sum, 1, MPI_FLOAT, sender, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                    (*sum) += his_sum;
-#ifdef DEBUG
-                    printf("[Step %d] : my_rank %d - Receive from %d - his_sum is %.2f and *sum is %.2f\n", step, my_rank, sender, his_sum, *sum);
-#endif
-                    break;
-                } else { /* Send */
-                    int receiver = j - pow(2, step);
-                    MPI_Send(sum, 1, MPI_FLOAT, receiver, 2, MPI_COMM_WORLD);
-#ifdef DEBUG
-                    printf("[Step %d] : my_rank %d - Send to %d - *sum is %.2f\n", step, my_rank, receiver, *sum);
-#endif
-                    if (++k == count - 1) // If I've just sent my last number.
-                        finish = 1; // Finish execution.
-                    break;
-                }
-            }
+            // Copy second half of `nums[]` to `my_nums`.
+            memcpy(my_nums, nums + qty, qty * sizeof(float));
 
-            if (n_proc % 2 == 0 && op < 0) { /* If the number of active processes is even current operation is a send. */
-                if (my_rank % 2 == 0) /* If I'm even. */
-                    op = -op; /* Next operation must be a `receive`. */
-                /* Otherwise, another `send` must be performed. */
-            } else { /* After a `receive` it does always come a `send`. */
-                op = -op; // Recv <=> Send.
-            }
+            // Send first half of `nums[]` to `dst`.
+            MPI_Send(nums, qty, MPI_FLOAT, dst, 2, MPI_COMM_WORLD);
+
+            // Receive second half of his `nums[]` into `his_nums`.
+            MPI_Recv(his_nums, qty, MPI_FLOAT, dst, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        } else {
+            int dst = my_rank - 1;
+
+            // Copy first half of `nums[]` to `my_nums`.
+            memcpy(my_nums, nums, qty * sizeof(float));
+
+            // Receive first half of his `nums[]` into `his_nums`.
+            MPI_Recv(his_nums, qty, MPI_FLOAT, dst, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            // Send second half of `nums[]` to `dst`.
+            MPI_Send(nums + qty, qty, MPI_FLOAT, dst, 2, MPI_COMM_WORLD);
         }
-    }
 
-    return 0;
+        // Sums the two vectors together.
+#ifdef DEBUG_REDUCE_SUMTREE
+       printf("#%d:", my_rank);
+#endif
+        for (int j = 0; j < qty; j++) {
+           res_nums[j] = my_nums[j] + his_nums[j]; 
+#ifdef DEBUG_REDUCE_SUMTREE
+           printf(" %.2f", res_nums[j]);
+#endif
+        }
+#ifdef DEBUG_REDUCE_SUMTREE
+           printf("\n");
+#endif
+
+        return reduce_sumtree(my_rank, comm_sz, qty, res_nums, sum);
+    }
 }
 
 
 void scather(int my_rank, int comm_sz, unsigned int *my_count, float **my_nums) {
+    if (my_rank == 0) {
+        unsigned int count;
+        scanf("%u", &count);  // Get numbers count.
+
+        // NOTE: It's better to use heap allocation to prevent stack overflows.
+        //      Another option would be increasing the stack size, which can be
+        //      done globally on Mac OS X with the following command:
+        //
+        //      ulimit -s hard
+        //
+        //      or by asking the linker with:
+        //
+        //      gcc ... -Wl,-stack_size,<stack_size> ...
+        //
+        //      where `<stack_size>` is a multiple of 4 (e.g.: 0xF0000000).
+        //
+        // float nums[count];
+        float *nums = (float *) malloc(count * sizeof(float));
+        for (int i = 0; i < count; i++) { // Get numbers.
+            scanf("%f", &nums[i]); 
+        }
+
+        div_t res = div(count, comm_sz);
+        // Process #0 computes the excedent if any (from 0 to *my_count - 1).
+        *my_count = res.quot + res.rem;
+        *my_nums = (float *) malloc((*my_count) * sizeof(float));
+        // NOTE: Uses `memcpy` instead of `for` for efficiency.
+        memcpy(*my_nums, nums, (*my_count) * sizeof(float));
+
+        // Divides array between other processes.
+        unsigned int dest_count = res.quot;
+        for (int dest = 1, index = *my_count; dest < comm_sz; dest++) {
+            float *dest_nums = (float *) calloc(dest_count, sizeof(float));
+            // NOTE: Uses `memcpy` instead of `for` for efficiency.
+            memcpy(dest_nums, nums + index, dest_count * sizeof(float));
+            index += dest_count;
+
+            MPI_Send(&dest_count, 1, MPI_UNSIGNED, dest, 0, MPI_COMM_WORLD);
+            MPI_Send(dest_nums, dest_count, MPI_FLOAT, dest, 1, MPI_COMM_WORLD);
+        }
+
+        free(nums);
+    } else { /* my_rank != 0 */
+        MPI_Recv(my_count, 1, MPI_UNSIGNED, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+        *my_nums = (float *) calloc(*my_count, sizeof(float));
+        MPI_Recv(*my_nums, *my_count, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+    }
+}
+
+
+void scather_intercalate(int my_rank, int comm_sz, unsigned int *my_count, float **my_nums) {
     if (my_rank == 0) {
         unsigned int count;
         scanf("%u", &count);  // Get numbers count.
@@ -184,12 +229,12 @@ void scather(int my_rank, int comm_sz, unsigned int *my_count, float **my_nums) 
 
         // Tell the processes how many numbers they're going to receive.
         *my_count = 0 < res.rem ? least_count + 1 : least_count;
-#ifdef DEBUG_SCATHER
+#ifdef DEBUG_SCATHER_INTERCALATE
         printf("#0 *my_count is: %u\n", *my_count);
 #endif
         for (int rank = 1; rank < comm_sz; rank++) {
             int rank_count = rank < res.rem ? least_count + 1 : least_count;
-#ifdef DEBUG_SCATHER
+#ifdef DEBUG_SCATHER_INTERCALATE
             printf("#%d *my_count is: %u\n", rank, rank_count);
 #endif
             MPI_Send(&rank_count, 1, MPI_UNSIGNED, rank, 0, MPI_COMM_WORLD);
@@ -204,7 +249,7 @@ void scather(int my_rank, int comm_sz, unsigned int *my_count, float **my_nums) 
                 (*my_nums)[j++] = nums[i];
             } else {
                 MPI_Send(&nums[i], 1, MPI_FLOAT, dest_rank, 1, MPI_COMM_WORLD);
-#ifdef DEBUG_SCATHER
+#ifdef DEBUG_SCATHER_INTERCALATE
                 printf("Sent to #%d number %.1f\n", dest_rank, nums[i]);
 #endif
             }
@@ -213,14 +258,14 @@ void scather(int my_rank, int comm_sz, unsigned int *my_count, float **my_nums) 
         free(nums);
     } else { /* my_rank != 0 */
         MPI_Recv(my_count, 1, MPI_UNSIGNED, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
-#ifdef DEBUG_SCATHER
+#ifdef DEBUG_SCATHER_INTERCALATE
         printf("\t#%d received *my_count: %u\n", my_rank, *my_count);
 #endif
         *my_nums = (float *) malloc((*my_count) * sizeof(float));
         // Receives the numbers assigning them to `*my_nums`.
         for (int i = 0; i < *my_count; i++) {
             MPI_Recv(&(*my_nums)[i], 1, MPI_FLOAT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
-#ifdef DEBUG_SCATHER
+#ifdef DEBUG_SCATHER_INTERCALATE
                 printf("\t#%d received number %.1f\n", my_rank, (*my_nums)[i]);
 #endif
         }
@@ -271,9 +316,9 @@ int main(int argc, char *argv[]) {
         float *his_nums;
         unsigned int his_count;
         for (int i = 1; i < comm_sz; i++) {
-            MPI_Recv(&his_count, 1, MPI_UNSIGNED, i, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&his_count, 1, MPI_UNSIGNED, i, 90, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             his_nums = (float *) malloc(his_count * sizeof(float));
-            MPI_Recv(his_nums, his_count, MPI_FLOAT, i, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(his_nums, his_count, MPI_FLOAT, i, 91, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
             printf("#%d:", i);
             for (int i = 0; i < his_count; i++) {
@@ -284,8 +329,8 @@ int main(int argc, char *argv[]) {
             free(his_nums);
         }
     } else {
-        MPI_Send(&my_count, 1, MPI_UNSIGNED, 0, 3, MPI_COMM_WORLD);
-        MPI_Send(my_nums, my_count, MPI_FLOAT, 0, 4, MPI_COMM_WORLD);
+        MPI_Send(&my_count, 1, MPI_UNSIGNED, 0, 90, MPI_COMM_WORLD);
+        MPI_Send(my_nums, my_count, MPI_FLOAT, 0, 91, MPI_COMM_WORLD);
     }
 #endif
 
@@ -295,7 +340,6 @@ int main(int argc, char *argv[]) {
     gettimeofday(&start, NULL);
 
     float sum;
-//    reduce(my_rank, comm_sz, my_count, my_nums, &sum);
     reduce_sumtree(my_rank, comm_sz, my_count, my_nums, &sum);
 
     // End
@@ -310,13 +354,13 @@ int main(int argc, char *argv[]) {
         ulli max_time = elapsed_ms;
         ulli his_elapsed;
         for (int i = 1; i < comm_sz; i++) {
-            MPI_Recv(&his_elapsed, 1, MPI_UNSIGNED_LONG_LONG, i, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&his_elapsed, 1, MPI_UNSIGNED_LONG_LONG, i, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             if (his_elapsed > max_time)
                 max_time = his_elapsed;
         }
         printf("%.2f %llu\n", sum, max_time);
     } else {
-        MPI_Send(&elapsed_ms, 1, MPI_UNSIGNED_LONG_LONG, 0, 3, MPI_COMM_WORLD);
+        MPI_Send(&elapsed_ms, 1, MPI_UNSIGNED_LONG_LONG, 0, 4, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
